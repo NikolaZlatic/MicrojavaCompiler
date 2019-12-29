dadf44dddad1d53fe22099decf2f9359c929faec
NikolaZlatic <nikola.zlatic@hotmail.com>
2019-12-29 15:11:11 +0100

    Add code for Lex analysis

---
+++ src/rs/ac/bg/etf/pp1/sym.java
@@ -0,0 +1,63 @@
+package rs.ac.bg.etf.pp1;
+
+public class sym {
+	
+	//KEYWORDS
+	public static final int PROG = 1;
+	public static final int BREAK = 2;
+	public static final int CLASS = 3;
+	public static final int ABSTRACT = 4;
+	public static final int ELSE = 5;
+	public static final int CONST = 6;
+	public static final int IF = 7;
+	public static final int NEW = 8;
+	public static final int PRINT = 9;
+	public static final int READ = 10;
+	public static final int RETURN = 11;
+	public static final int VOID = 12;
+	public static final int FOR = 13;
+	public static final int EXTENDS = 14;
+	public static final int CONTINUE = 15;
+	
+	//IDENTIFICATORS
+	public static final int IDENT = 16;
+	
+	//CONSTANTS
+	public static final int INT = 46;
+	public static final int NUMBER = 17;
+	public static final int CHARPRE = 47;
+	public static final int CHAR = 18;
+	public static final int BOOLPRE = 48;
+	public static final int BOOL = 19;
+	
+	//OPERATORS
+	public static final int PLUS = 20;
+	public static final int MINUS = 21;
+	public static final int TIMES = 22;
+	public static final int DEVIDE = 23;
+	public static final int MOD = 24;
+	public static final int IDENTICAL = 25;
+	public static final int NOTIDENTICAL = 26;
+	public static final int GREATER = 27;
+	public static final int GREATEREQUAL = 28;
+	public static final int LESS = 29;
+	public static final int LESSEQUAL = 30;
+	public static final int AND = 31;
+	public static final int OR = 32;
+	public static final int EQUAL = 33;
+	public static final int PLUSPLUS = 34;
+	public static final int MINUSMINUS = 35;
+	public static final int SEMI = 36;
+	public static final int COMMA = 37;
+	public static final int DOT = 38;
+	public static final int LPAREN = 39;
+	public static final int RPAREN = 40;
+	public static final int LBRACE = 41;
+	public static final int RBRACE = 42;
+	public static final int LSQUARE = 43;
+	public static final int RSQUARE = 44;
+	
+	
+	//OTHERS
+	public static final int EOF = 45;
+}
